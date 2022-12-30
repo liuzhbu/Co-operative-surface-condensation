ffbc3898f933a729d73ca61f4c64de1b2b193e95
liuzhbu <54942895+liuzhbu@users.noreply.github.com>
2022-12-30 07:01:25 -0800

    Update IDP9mer_bulk_simu_functions.py

--- GCMC_Simulation/Obstacle-Simulations/IDP9mer_bulk_simu_functions.py
+++ GCMC_Simulation/Obstacle-Simulations/IDP9mer_bulk_simu_functions.py
@@ -291,19 +291,3 @@ def Plot_3D_lattice_memb(lattice_array, polymerpool, memarr,tetharr, D,L, polyml
     if latfigname:
         latt_fig.savefig(latfigname)
     plt.close(latt_fig)
-
-
-def x_rotation(vector,theta):
-    """Rotates 3-D vector around x-axis"""
-    R = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0, np.sin(theta), np.cos(theta)]])
-    return np.dot(R,vector)
-
-def y_rotation(vector,theta):
-    """Rotates 3-D vector around y-axis"""
-    R = np.array([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta), 0, np.cos(theta)]])
-    return np.dot(R,vector)
-
-def z_rotation(vector,theta):
-    """Rotates 3-D vector around z-axis"""
-    R = np.array([[np.cos(theta), -np.sin(theta),0],[np.sin(theta), np.cos(theta),0],[0,0,1]])
-    return np.dot(R,vector)
